1d5ba0240135a20acba0962e8616b9f2df5c3276
Jan Henckens <jan@statik.be>
2021-09-23 09:40:30 +0200

    Icon & basic scaffolding

---
+++ src/CookieBanner.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace statikbe\cookiebanner;
+
+use craft\base\Plugin;
+
+class CookieBanner extends Plugin
+{
+	 public function init()
+    {
+        parent::init();
+       
+    }
+}
