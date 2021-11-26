6b0e4b168cde5724fe9e7574fe3514e3a0cf402c
Jan Henckens <jan@statik.be>
2021-11-26 16:47:15 +0100

    Added event to register plugin translations when statikbe/craft-translate is enabled

--- src/translations/nl-BE/cookie-banner.php
+++ src/translations/nl-BE/cookie-banner.php
@@ -1,23 +1,21 @@
 <?php
 
-return [
-    // Banner
-    'Deze website gebruikt cookies' => 'Deze website gebruikt cookies',
-    'Door op “Alle cookies aanvaarden” te klikken ga je akkoord met het opslaan van cookies om je een betere gebruikerservaring te bieden en websitegebruik te analyseren. Noodzakelijke cookies zijn essentieel om de website goed te doen functioneren en kan je niet weigeren. Door op "Cookie-instellingen" te klikken kan je je voorkeuren beheren.' => 'Door op “Alle cookies aanvaarden” te klikken ga je akkoord met het opslaan van cookies om je een betere gebruikerservaring te bieden en websitegebruik te analyseren. Noodzakelijke cookies zijn essentieel om de website goed te doen functioneren en kan je niet weigeren. Door op "Cookie-instellingen" te klikken kan je je voorkeuren beheren.',
-    'Alle cookies aanvaarden' => 'Alle cookies aanvaarden',
-    'Noodzakelijke cookies aanvaarden' => 'Noodzakelijke cookies aanvaarden',
-    'Cookieinstellingen' => 'Cookieinstellingen',
-    // Modal
-    'Sluit cookie instellingen' => 'Sluit cookie instellingen',
-    'Onze website houdt vier niveaus van cookies bij. Je kan zelf je voorkeuren op elk moment aanpassen. Meer informatie over wat cookies zijn en welke cookies we verzamelen kan je lezen in ons <a href="{cookieLink}" target="_blank" class="underline hover:no-underline">cookiebeleid</a>.' => 'Onze website houdt vier niveaus van cookies bij. Je kan zelf je voorkeuren op elk moment aanpassen. Meer informatie over wat cookies zijn en welke cookies we verzamelen kan je lezen in ons <a href="{cookieLink}" target="_blank" class="underline hover:no-underline">cookiebeleid</a>.',
-    'Of pas je voorkeuren aan' => 'Of pas je voorkeuren aan',
-    'Essentiële cookies' => 'Essentiële cookies',
-    'zijn om puur technische redenen nodig om de website te kunnen bezoeken.' => 'zijn om puur technische redenen nodig om de website te kunnen bezoeken.',
-    'Functionele cookies' => 'Functionele cookies',
-    'zijn noodzakelijk voor het gebruiken van de website.' => 'zijn noodzakelijk voor het gebruiken van de website.',
-    'Analytische cookies' => 'Analytische cookies',
-    'laten ons toe om het gebruik van de website te analyseren en de bezoekerservaring te verbeteren.' => 'laten ons toe om het gebruik van de website te analyseren en de bezoekerservaring te verbeteren.',
-    'Marketingcookies' => 'Marketing cookies',
-    'hebben als doel om je ervaring te kunnen personaliseren en je relevante inhoud en aanbiedingen te sturen op deze en andere websites.' => 'hebben als doel om je ervaring te kunnen personaliseren en je relevante inhoud en aanbiedingen te sturen op deze en andere websites.',
-    'Mijn keuze bewaren' => 'Mijn keuze bewaren',
-];
+return array (
+	'Deze website gebruikt cookies' => 'Deze website gebruikt cookiessssss',
+	'Door op “Alle cookies aanvaarden” te klikken ga je akkoord met het opslaan van cookies om je een betere gebruikerservaring te bieden en websitegebruik te analyseren. Noodzakelijke cookies zijn essentieel om de website goed te doen functioneren en kan je niet weigeren. Door op "Cookie-instellingen" te klikken kan je je voorkeuren beheren.' => 'Door op “Alle cookies aanvaarden” te klikken ga je akkoord met het opslaan van cookies om je een betere gebruikerservaring te bieden en websitegebruik te analyseren. Noodzakelijke cookies zijn essentieel om de website goed te doen functioneren en kan je niet weigeren. Door op "Cookie-instellingen" te klikken kan je je voorkeuren beheren.',
+	'Alle cookies aanvaarden' => 'Alle cookies aanvaarden',
+	'Noodzakelijke cookies aanvaarden' => 'Noodzakelijke cookies aanvaarden',
+	'Cookieinstellingen' => 'Cookieinstellingen',
+	'Sluit cookie instellingen' => 'Sluit cookie instellingen',
+	'Onze website houdt vier niveaus van cookies bij. Je kan zelf je voorkeuren op elk moment aanpassen. Meer informatie over wat cookies zijn en welke cookies we verzamelen kan je lezen in ons <a href="{cookieLink}" target="_blank" class="underline hover:no-underline">cookiebeleid</a>.' => 'Onze website houdt vier niveaus van cookies bij. Je kan zelf je voorkeuren op elk moment aanpassen. Meer informatie over wat cookies zijn en welke cookies we verzamelen kan je lezen in ons <a href="{cookieLink}" target="_blank" class="underline hover:no-underline">cookiebeleid</a>.',
+	'Of pas je voorkeuren aan' => 'Of pas je voorkeuren aan',
+	'Essentiële cookies' => 'Essentiële cookies',
+	'zijn om puur technische redenen nodig om de website te kunnen bezoeken.' => 'zijn om puur technische redenen nodig om de website te kunnen bezoeken.',
+	'Functionele cookies' => 'Functionele cookies',
+	'zijn noodzakelijk voor het gebruiken van de website.' => 'zijn noodzakelijk voor het gebruiken van de website.',
+	'Analytische cookies' => 'Analytische cookies',
+	'laten ons toe om het gebruik van de website te analyseren en de bezoekerservaring te verbeteren.' => 'laten ons toe om het gebruik van de website te analyseren en de bezoekerservaring te verbeteren.',
+	'Marketingcookies' => 'Marketingcookies',
+	'hebben als doel om je ervaring te kunnen personaliseren en je relevante inhoud en aanbiedingen te sturen op deze en andere websites.' => 'hebben als doel om je ervaring te kunnen personaliseren en je relevante inhoud en aanbiedingen te sturen op deze en andere websites.',
+	'Mijn keuze bewaren' => 'Mijn keuze bewaren',
+);
